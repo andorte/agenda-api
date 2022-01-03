c3b206900a732dcd1870067de69d893ce6079bc6
ANDRE DORTE <andorte@gmail.com>
2022-01-03 13:23:41 -0300

    Refactor. Extract controllers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const consign = require('consign')
 
 const app = express()
 
-app.listen(3000, () => console.log('running on 3000 port'))
+consign()
+    .include('controllers')
+    .into(app)
 
-app.get('/', (req, res) => res.send('Servidor rodando'))
+app.listen(3000, () => console.log('running on 3000 port'))
