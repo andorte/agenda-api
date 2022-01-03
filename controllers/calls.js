b228f0a0672780080a878f375a3392ffc5288c12
ANDRE DORTE <andorte@gmail.com>
2022-01-03 16:54:24 -0300

    Add calls requests paths

--- controllers/calls.js
+++ controllers/calls.js
@@ -1,3 +1,8 @@
 module.exports = app => {
-    app.get('/', (req, res) => res.send('Servidor rodando'))
+    app.get('/', (req, res) => res.send('Servidor rodando')),
+    app.get('/calls', (req, res) => res.send('Atendimento')),
+    app.post('/calls', (req, res) => {
+        console.log(req.body)
+        res.send('Atendimento (post)')
+    })
 }
