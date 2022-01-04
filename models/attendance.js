1e50f5620abecb06f83d920514d7635e8c27deac
ANDRE DORTE <andorte@gmail.com>
2022-01-04 16:40:59 -0300

    Validate attendance fields

--- models/attendance.js
+++ models/attendance.js
@@ -6,17 +6,40 @@ class Attendance {
     add(attendance, res) {
         const createTime = moment().format(bdFormat)
         const date = moment(attendance.date, "DD/MM/YYYY").format(bdFormat)
-        const datedAttendance = {...attendance, createTime, date}
-        const sql = 'INSERT into Attendances SET ?'
 
-        connection.query(sql, datedAttendance, (error, results) => {
-            if (error) {
-                res.status(400).json(error)
-            } else {
-                res.status(201).json(results)
+        const isDateValid = moment(date).isSameOrAfter(createTime)
+        const isClientNameValid = attendance.client.length >= 5
+
+        const validations = [
+            {
+                name: 'date',
+                valid: isDateValid,
+                message: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                name: 'client',
+                valid: isClientNameValid,
+                message: 'Cliente deve ter pelo menos cinco caracteres'
             }
+        ]
 
-        })
+        const errors = validations.filter(validation => !validation.valid)
+
+        if (errors.length) {
+            res.status(400).json(errors)
+        } else {
+            const datedAttendance = {...attendance, createTime, date}
+            const sql = 'INSERT into Attendances SET ?'
+
+            connection.query(sql, datedAttendance, (error, results) => {
+                if (error) {
+                    res.status(400).json(error)
+                } else {
+                    res.status(201).json(results)
+                }
+
+            })
+        }
     }
 }
 
