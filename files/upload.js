36bd0294a57403c5e4c544170da0213d46cab15d
ANDRE DORTE <andorte@gmail.com>
2022-01-07 14:54:17 -0300

    Handle file type on pet image upload

--- files/upload.js
+++ files/upload.js
@@ -1,11 +1,19 @@
 const fs = require('fs')
+const p = require('path')
 
 module.exports = (path, fileName, createdImageCallback) => {
-    const newPath = `./assets/images/${fileName}`
+    const validTypes = ['jpg', 'png', 'jpeg']
+    const type = p.extname(path)
 
-    fs.createReadStream(path).pipe(
-        fs.createWriteStream(newPath)
-    ).on('finish', () => {
-        createdImageCallback(newPath)
-    })
+    if (validTypes.indexOf(type.substring(1)) === -1) {
+        const error = "Tipo é inválido"
+        createdImageCallback(error)
+    } else {
+        const newPath = `./assets/images/${fileName}${type}`
+        fs.createReadStream(path).pipe(
+            fs.createWriteStream(newPath)
+        ).on('finish', () => {
+            createdImageCallback(false, newPath)
+        })
+    }
 }
--- models/pets.js
+++ models/pets.js
@@ -3,16 +3,20 @@ const upload = require('../files/upload')
 
 class Pet {
     add(pet, res) {
-        upload(pet.image, pet.name, newPath => {
-            const query = 'INSERT INTO Pets SET ?'
-            pet.image = newPath
-            connection.query(query, pet, error => {
-                if (error) {
-                    res.status(400).json(error)
-                } else {
-                    res.status(200).json(pet)
-                }
-            })
+        upload(pet.image, pet.name, (error, newPath) => {
+            if (error) {
+                res.status(400).json({error})
+            } else {
+                const query = 'INSERT INTO Pets SET ?'
+                pet.image = newPath
+                connection.query(query, pet, error => {
+                    if (error) {
+                        res.status(400).json(error)
+                    } else {
+                        res.status(200).json(pet)
+                    }
+                })
+            }
         })
     }
 }
